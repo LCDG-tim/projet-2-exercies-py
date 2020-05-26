7598f8dc9c996d4316369ac2cd5b4e589842dcda
Timothe Lebobe <timothe.lebobe@gmail.com>
2020-05-26 23:50:10 +0200

    ex 4 assertion

--- ex4.py
+++ ex4.py
@@ -12,9 +12,16 @@ def ex4(n: int, m: int, days_f: list) -> int:
     assert isinstance(days_f, list), \
         'days_f must be a list, not {}'.format(type(days_f))
     assert 1 <= n <= 100, \
-        "n must be between 1 and 100 includes, not {}".format(n)
+        "n must be between 1 and 100    includes, not {}".format(n)
     assert 1 <= m <= 36, \
         "m must be between 1 and 36 includes, not {}".format(m)
+    assert len(days_f) == n, \
+        "il n'y a pas le même nombre d'amis que de disponibilité {} au lieu " \
+        "de {}".format(len(days_f), n)
+    for j, i in enumerate(days_f):
+        assert 1 <= i <= 10 ** 6, \
+            "l'élément {} de days_f n'est pas compris entre 1 et 1000" \
+            "".format(j, i)
 
     amis_vu = 0
     start = days_f[0]
@@ -36,8 +43,15 @@ def ex4(n: int, m: int, days_f: list) -> int:
 
 
 if __name__ == "__main__":
+    # 1ere ligne d'entrée nb_ami et nobre de jours de voyage
     nb_amis, nb_jours = input().split(" ")
+    # transition str -> int
     nb_amis: int = int(nb_amis)
     nb_jours: int = int(nb_jours)
-    day_with_friends: list = sorted([int(i) for i in input().split()])
+    # 2e ligne entrée liste d'entiers séparés par un espace
+    day_with_friends: list = sorted([int(i) for i in input().split(" ")])
+    # le programme appelle la fonction
     print(ex4(nb_amis, nb_jours, day_with_friends))
+
+    # nettoyage de la mémoire vive
+    del nb_amis, nb_jours, ex4, day_with_friends
